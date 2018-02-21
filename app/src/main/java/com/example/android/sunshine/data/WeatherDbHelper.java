015f01f8d2c2adacfae86fc6b8645ee071babce6
Daniel Burgner <dburgnerjr@yahoo.com>
2018-02-20 20:40:34 -0600

    D51 Lesson 8 Round 2 Complete.
    D51 of #100DaysOfCode.  Finished second round of Lesson 8 of
    #GrowWithGoogle #AndroidDev phase one scholarship program sponsored by
    Google and Udacity.  #GoogleUdacityScholars

---
+++ app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
@@ -0,0 +1,54 @@
+package com.example.android.sunshine.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.android.sunshine.data.WeatherContract.WeatherEntry;
+
+/**
+ * Manages a local database for weather data.
+ */
+public class WeatherDbHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "weather.db";
+    private static final int  DATABASE_VERSION = 3;
+
+    public WeatherDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        final String SQL_CREATE_WAITLIST_TABLE =
+                "CREATE TABLE " + WeatherEntry.TABLE_NAME + " (" +
+
+                /*
+                 * WeatherEntry did not explicitly declare a column called "_ID". However,
+                 * WeatherEntry implements the interface, "BaseColumns", which does have a field
+                 * named "_ID". We use that here to designate our table's primary key.
+                 */
+                WeatherEntry._ID               + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        WeatherEntry.COLUMN_DATE       + " INTEGER NOT NULL, "                 +
+
+                        WeatherEntry.COLUMN_WEATHER_ID + " INTEGER NOT NULL, "                 +
+
+                        WeatherEntry.COLUMN_MIN_TEMP   + " REAL NOT NULL, "                    +
+                        WeatherEntry.COLUMN_MAX_TEMP   + " REAL NOT NULL, "                    +
+
+                        WeatherEntry.COLUMN_HUMIDITY   + " REAL NOT NULL, "                    +
+                        WeatherEntry.COLUMN_PRESSURE   + " REAL NOT NULL, "                    +
+
+                        WeatherEntry.COLUMN_WIND_SPEED + " REAL NOT NULL, "                    +
+                        WeatherEntry.COLUMN_DEGREES    + " REAL NOT NULL" +
+                        " UNIQUE (" + WeatherEntry.COLUMN_DATE + ") ON CONFLICT REPLACE);";
+
+        sqLiteDatabase.execSQL(SQL_CREATE_WAITLIST_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + WeatherEntry.TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+
+}
